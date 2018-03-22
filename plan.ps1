2bff89dbe9a38652c8004e10ba5d594b716e6e12
skylerto <skylerclayne@gmail.com>
2018-03-22 14:28:45 +0000

    install pip
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- plan.ps1
+++ plan.ps1
@@ -4,22 +4,25 @@ $pkg_version = "2.7.14"
 $pkg_maintainer = "Indellient Inc. <skylerl@indellient.com>"
 $pkg_description = "Installs python2 for windows"
 $pkg_upstream_url = "https://www.python.org/ftp/python/$pkg_version/python-$pkg_version.msi"
-$pkg_bin_dirs = @("bin")
+$pkg_bin_dirs = @("bin", "bin/Scripts")
 
 function Invoke-Download {
   Write-Host "Downloading $pkg_upstream_url"
   Invoke-WebRequest -URI $pkg_upstream_url -OutFile "$HAB_CACHE_SRC_PATH\python-$pkg_version.msi"
+  Invoke-WebRequest -URI "https://bootstrap.pypa.io/get-pip.py" -OutFile "$HAB_CACHE_SRC_PATH\get-pip.py"
 }
 function Invoke-Verify { }
 
 function Invoke-Build { }
 
 function Invoke-Install {
-  Write-Host "Installing to $pkg_prefix\bin"
-  $dir="C:\Python27"
+  $dir="$pkg_prefix\bin"
+  Write-Host "Installing python binaries to $dir"
 
-  Write-Host "*** DOES NOT OBEY TARGETDIR=$pkg_prefix\bin, YOU WILL GET AN INSTALLATIoN OF PYTHON HERE $dir ***"
-  Start-Process -Wait -FilePath msiexec -ArgumentList /i, "$HAB_CACHE_SRC_PATH\python-$pkg_version.msi TARGETDIR=$dir", /qn, /l*v, "$PLAN_CONTEXT\install.log"
+  Start-Process -Wait -FilePath msiexec -ArgumentList /a, "$HAB_CACHE_SRC_PATH\python-$pkg_version.msi TARGETDIR=$dir  ADDLOCAL=pip", "ALLUSERS=1" , /qn
 
-  Copy-Item $dir\* -Destination  $pkg_prefix\bin -Recurse
+  $env:Path = "$pkg_prefix\bin";
+
+  Write-Host "Installing pip to $dir\Scripts"
+  Start-Process -Wait -FilePath python -ArgumentList $HAB_CACHE_SRC_PATH\get-pip.py
 }
